826e763918d09254421c60b35a960025fd2d4438
Brian O'Donnell <brian.odonnell@hansoninc.com>
2020-02-28 11:53:06 -0500

    Made work page dynamic

--- 2020_website/themes/2020_Website/page-work.php
+++ 2020_website/themes/2020_Website/page-work.php
@@ -17,9 +17,39 @@
 						<h1>Work</h1>
 					</div>
 
-					<!-- <p data-aos="fade-up">This is some test content to just see if the photography page is working correctly with the rest of the pages. It looked like it wasn't transitioning like the others.</p> -->
-
 					<div class="work__gallery">
+
+						<?php
+							$args = array(
+								'post_type' => array('work'),
+								'post_status' => array('publish'),
+								'nopaging' => true,
+								'order' => 'DESC',
+								'orderby' => 'date'
+							);
+							$work = new WP_Query($args);
+						?>
+
+						<?php if ($work->have_posts()) {
+							while ($work->have_posts()) {
+								$work->the_post(); ?>
+
+								<div class="work__item">
+									<a href="<?php echo get_permalink(); ?>" alt="<?php the_field('client'); ?>" class="internal-link">
+										<div class="work__image">
+											<img src="<?php the_field('thumbnail'); ?>">
+										</div>
+										<div class="work__description">
+											<img src="<?php the_field('client_logo'); ?>" alt="<?php the_field('client'); ?>" class="feature__logo">
+											<h3 class="feature__title"><?php the_field('project_title'); ?></h3>
+										</div>
+									</a>
+								</div>
+
+							<?php }
+						} ?>
+						<?php wp_reset_query(); ?>
+
 						<div class="work__item">
 							<div class="work__image">
 								<img src="https://picsum.photos/1920/1080">
@@ -29,6 +59,7 @@
 								<h3 class="feature__title">Website rebuild</h3>
 							</div>
 						</div>
+
 						<div class="work__item">
 							<div class="work__image">
 								<img src="https://picsum.photos/1920/1080">
