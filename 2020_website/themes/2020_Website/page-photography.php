b521a32653e06f092aac3fa0251a374a0358ca84
Brian O'Donnell <>
2020-02-27 21:13:20 -0500

    Statically built Work and Photo pages

--- 2020_website/themes/2020_Website/page-photography.php
+++ 2020_website/themes/2020_Website/page-photography.php
@@ -9,19 +9,116 @@
 ?>
 
 <main data-router-wrapper>
-	<div data-router-view="photography" id="page" class="photography">
-		<div class="wrapper">
+	<div data-router-view="photo" id="page" class="photo">
+		<div class="photo__container">
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-12">
-						<div class="work-content">
-							<h1>PHOTOGRAPHY</h1>
+					<div class="photo__intro" data-aos="fade-up">
+						<h1>Photography</h1>
+					</div>
 
-							<p data-aos="fade-up">This is some test content to just see if the photography page is working correctly with the rest of the pages. It looked like it wasn't transitioning like the others.</p>
+					<!-- <p data-aos="fade-up">This is some test content to just see if the photography page is working correctly with the rest of the pages. It looked like it wasn't transitioning like the others.</p> -->
+
+					<div class="photo__gallery">
+						<div class="photo__item">
+							<div class="photo__image">
+								<img src="https://picsum.photos/id/1/1920/1080">
+								<img src="https://picsum.photos/id/2/1920/1080">
+								<img src="https://picsum.photos/id/3/1920/1080">
+							</div>
+							<div class="photo__description">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+						<div class="photo__item">
+							<div class="photo__image">
+								<img src="https://picsum.photos/id/1/1920/1080">
+								<img src="https://picsum.photos/id/2/1920/1080">
+								<img src="https://picsum.photos/id/3/1920/1080">
+							</div>
+							<div class="photo__description">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="photo__item">
+							<div class="photo__image">
+								<img src="https://picsum.photos/id/1/1920/1080">
+								<img src="https://picsum.photos/id/2/1920/1080">
+								<img src="https://picsum.photos/id/3/1920/1080">
+							</div>
+							<div class="photo__description">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="photo__item">
+							<div class="photo__image">
+								<img src="https://picsum.photos/id/1/1920/1080">
+								<img src="https://picsum.photos/id/2/1920/1080">
+								<img src="https://picsum.photos/id/3/1920/1080">
+							</div>
+							<div class="photo__description">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="photo__item">
+							<div class="photo__image">
+								<img src="https://picsum.photos/id/1/1920/1080">
+								<img src="https://picsum.photos/id/2/1920/1080">
+								<img src="https://picsum.photos/id/3/1920/1080">
+							</div>
+							<div class="photo__description">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="photo__item">
+							<div class="photo__image">
+								<img src="https://picsum.photos/id/1/1920/1080">
+								<img src="https://picsum.photos/id/2/1920/1080">
+								<img src="https://picsum.photos/id/3/1920/1080">
+							</div>
+							<div class="photo__description">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="photo__item">
+							<div class="photo__image">
+								<img src="https://picsum.photos/id/1/1920/1080">
+								<img src="https://picsum.photos/id/2/1920/1080">
+								<img src="https://picsum.photos/id/3/1920/1080">
+							</div>
+							<div class="photo__description">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="photo__item">
+							<div class="photo__image">
+								<img src="https://picsum.photos/id/1/1920/1080">
+								<img src="https://picsum.photos/id/2/1920/1080">
+								<img src="https://picsum.photos/id/3/1920/1080">
+							</div>
+							<div class="photo__description">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="photo__item">
+							<div class="photo__image">
+								<img src="https://picsum.photos/id/1/1920/1080">
+								<img src="https://picsum.photos/id/2/1920/1080">
+								<img src="https://picsum.photos/id/3/1920/1080">
+							</div>
+							<div class="photo__description">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
 						</div>
 					</div>
 				</div>
-
 			</div>
 		</div>
 	</div>
--- 2020_website/themes/2020_Website/page-work.php
+++ 2020_website/themes/2020_Website/page-work.php
@@ -10,18 +10,106 @@
 
 <main data-router-wrapper>
 	<div data-router-view="work" id="page" class="work">
-		<div class="wrapper">
+		<div class="work__container">
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-12">
-						<div class="work-content">
-							<h1>WORK</h1>
+					<div class="work__intro" data-aos="fade-up">
+						<h1>Work</h1>
+					</div>
 
-							<p data-aos="fade-up">This is some test content to just see if the photography page is working correctly with the rest of the pages. It looked like it wasn't transitioning like the others.</p>
+					<!-- <p data-aos="fade-up">This is some test content to just see if the photography page is working correctly with the rest of the pages. It looked like it wasn't transitioning like the others.</p> -->
+
+					<div class="work__gallery">
+						<div class="work__item">
+							<div class="work__image">
+								<img src="https://picsum.photos/1920/1080">
+							</div>
+							<div class="work__description">
+								<img src="/wp-content/themes/2020_Website/assets/images/logos/hanson.png" alt="Hanson" class="feature__logo">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+						<div class="work__item">
+							<div class="work__image">
+								<img src="https://picsum.photos/1920/1080">
+							</div>
+							<div class="work__description">
+								<img src="/wp-content/themes/2020_Website/assets/images/logos/hanson.png" alt="Hanson" class="feature__logo">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="work__item">
+							<div class="work__image">
+								<img src="https://picsum.photos/1920/1080">
+							</div>
+							<div class="work__description">
+								<img src="/wp-content/themes/2020_Website/assets/images/logos/hanson.png" alt="Hanson" class="feature__logo">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="work__item">
+							<div class="work__image">
+								<img src="https://picsum.photos/1920/1080">
+							</div>
+							<div class="work__description">
+								<img src="/wp-content/themes/2020_Website/assets/images/logos/hanson.png" alt="Hanson" class="feature__logo">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="work__item">
+							<div class="work__image">
+								<img src="https://picsum.photos/1920/1080">
+							</div>
+							<div class="work__description">
+								<img src="/wp-content/themes/2020_Website/assets/images/logos/hanson.png" alt="Hanson" class="feature__logo">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="work__item">
+							<div class="work__image">
+								<img src="https://picsum.photos/1920/1080">
+							</div>
+							<div class="work__description">
+								<img src="/wp-content/themes/2020_Website/assets/images/logos/hanson.png" alt="Hanson" class="feature__logo">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="work__item">
+							<div class="work__image">
+								<img src="https://picsum.photos/1920/1080">
+							</div>
+							<div class="work__description">
+								<img src="/wp-content/themes/2020_Website/assets/images/logos/hanson.png" alt="Hanson" class="feature__logo">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="work__item">
+							<div class="work__image">
+								<img src="https://picsum.photos/1920/1080">
+							</div>
+							<div class="work__description">
+								<img src="/wp-content/themes/2020_Website/assets/images/logos/hanson.png" alt="Hanson" class="feature__logo">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
+						</div>
+
+						<div class="work__item">
+							<div class="work__image">
+								<img src="https://picsum.photos/1920/1080">
+							</div>
+							<div class="work__description">
+								<img src="/wp-content/themes/2020_Website/assets/images/logos/hanson.png" alt="Hanson" class="feature__logo">
+								<h3 class="feature__title">Website rebuild</h3>
+							</div>
 						</div>
 					</div>
 				</div>
-
 			</div>
 		</div>
 	</div>
